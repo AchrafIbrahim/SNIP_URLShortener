52deff5aca77ed4189b702361c79ee6f1057a5a0
IbrahMaula01-github <maulaibrah@gmail.com>
2026-06-08 13:18:31 +0700

    edit file main.go to connect to db snip, create table links,  and testing the redirect URL slug

--- main.go
+++ main.go
@@ -1,19 +1,54 @@
 package main
 
 import (
-	"net/http"
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	_"github.com/lib/pq"
 )
 
+var db *sql.DB
+
+func initDB() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	dsn:= fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+	)
+	db, err = sql.Open("postgres", dsn)
+	if err != nil {
+		log.Fatal("Cannot reach database:", err)
+	}
+
+	fmt.Println("Database connected successfully!")
+}
+
 func main() {
+	initDB()
+
 	r := gin.Default()
 
 	r.GET("/:slug", func(c *gin.Context) {
 		slug := c.Param("slug")
-		c.JSON(http.StatusOK, gin.H{
-			"slug": slug,
-		})
+
+		var originalURL string
+		err := db.QueryRow("SELECT original_url FROM links WHERE slug = $1", slug).Scan(&originalURL)
+		if err != nil {
+			c.JSON(404, gin.H{"error": "Link tidak ditemukan"})
+			return
+		}
+
+		c.Redirect(302, originalURL)
 	})
 
 	r.Run(":9090")
